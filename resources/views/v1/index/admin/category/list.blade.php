01dd1dd4241b334598f9fd66e01e4226e309352e
Reza1151 <rezaalimohamdyr@gmail.com>
2024-02-15 21:24:48 +0330

    finito

--- resources/views/v1/index/admin/category/list.blade.php
+++ resources/views/v1/index/admin/category/list.blade.php
@@ -3,15 +3,32 @@
 <div>
     <h2> لیست دسته بندی ها</h2>
     <table>
+        @if(Session::has('category_success'))
+            <div class="alert alert-success">
+                <div>{{session('category_success')}}</div>
+            </div>
+        @endif
+        @if(Session::has('category_error'))
+            <div class="alert alert-success">
+                <div>{{session('category_error')}}</div>
+            </div>
+        @endif
         <tr class="tableListCategory">
             <th class="thListCategory" colspan="2"> ردیف</th>
             <th class="thListCategory" colspan="2"> نام</th>
             <th class="thListCategory" colspan="2"> عملیات</th>
         </tr>
+        @foreach($categories as $row)
         <tr>
             <td class="tableListCategory" colspan="2"> 1</td>
             <td class="tableListCategory" colspan="2"> dxcdsc</td>
             <td class="tableListCategory" colspan="2">
+                <form action="{{route('categories.destroy',$row->id)}}" method="post">
+                    {{csrf_field()}}
+                    {{method_field('DELETE')}}
+                    <button type="submit">DE</button>
+
+                </form>
                 <img src="{{asset('assets/v1/admin/image/visible.png')}}" alt="" usemap="#photo1">
                 <img src="{{asset('assets/v1/admin/image/delete.png')}}" alt="" usemap="#photo1">
                 <img src="{{asset('assets/v1/admin/image/pencil.png')}}" alt="" usemap="#photo1">
@@ -19,6 +36,9 @@
             <map name="photo1">
                 <area shape="circle" coords="14,12,13" href="" alt="" target="_blank" title="">
             </map>
+            @if(count($row->childrenRecursive)>0)
+                @include('index.admin.category.subaddCategory',['category'=>$row->childrenRecursive , 'level'=>$level+1])
+            @endforeach
         </tr>
         <tr class="tableListCategory">
             <td class="tableListCategory" colspan="2"> 2</td>
